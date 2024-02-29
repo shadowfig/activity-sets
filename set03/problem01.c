7bbe7f64d99b2f5b08f861a0efcd0097d86e2043
Kunal Aley <kunala.bsc23@rvu.edu.in>
2024-02-29 21:37:18 +0530

    test

---
+++ set03/problem01.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+void input(float *x1, float *y1, float *x2, float *y2);
+float find_distance(float x1, float y1, float x2, float y2);
+void output(float x1, float y1, float x2, float y2, float distance);
+
+int main()
+{
+    float x1,y1,x2,y2,distance;
+    input(&x1,&y1,&x2,&y2);
+    distance=find_distance(x1,y1,x2,y2);
+    output(x1,y1,x2,y2,distance);
+}
+void input(float *x1, float *y1, float *x2, float *y2)
+{
+    printf("Enter the point for x1 : \n");
+    scanf("%f",x1);
+    printf("Enter the point for y1 : \n");
+    scanf("%f",y1);
+    printf("Enter the point for x2 : \n");
+    scanf("%f",x2);
+    printf("Enter the point for y2 : \n");
+    scanf("%f",y2);
+}
+float find_distance(float x1, float y1, float x2, float y2)
+{
+    float distance;
+    distance=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+    return distance;
+}
+void output(float x1, float y1, float x2, float y2, float distance)
+{
+    printf("The distance between point (%f,%f) and (%f,%f) is %f",x1,x2,y1,y2,distance);
+}
