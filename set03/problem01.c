7b4a4cd6cc50ccc36deb6bf14e7aa3f25d74f93c
Kunal Aley <kunala.bsc23@rvu.edu.in>
2024-03-01 18:22:02 +0530

    test

--- set03/problem01.c
+++ set03/problem01.c
@@ -23,7 +23,7 @@ void input(float *x1, float *y1, float *x2, float *y2)
     printf("Enter the point for y2 : \n");
     scanf("%f",y2);
 }
-float find_distance(float x1, float y1, float x2, float y2
+float find_distance(float x1, float y1, float x2, float y2)
 {
     float distance;
     distance=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
